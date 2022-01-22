52cbe3efddcc535e00d0c8b45eaecbaa81f7807c
Gokulnath <Gokulaps1998@gmail.com>
2022-01-22 20:47:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { QuicklinksComponent } from './quicklinks/quicklinks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    DashboardComponent,
+    QuicklinksComponent
   ],
   imports: [
     BrowserModule,
